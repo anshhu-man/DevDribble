e956d5705a795326dc9326269842f50d7e53aa48
anshhu-man <101041684+anshhu-man@users.noreply.github.com>
2022-03-07 13:30:35 +0530

    Update validate.js

--- validate.js
+++ validate.js
@@ -2,9 +2,10 @@ function validate(){
 var username=document.getElementById("username").value;
 var password=document.getElementById("password").value;
 if(username=="Anshuman"&& password=="7872"){
-alert("You are Successfully Logged in");
-windows.open("https://www.youtube.com/watch?v=I2weoKK_vT4");
+    alert("You are Successfully Logged in");
+    window.open("https://www.youtube.com/watch?v=I2weoKK_vT4")
+    return false;
 }
 else
-alert("Who are you?");
+    alert("Who are you?");
 }
